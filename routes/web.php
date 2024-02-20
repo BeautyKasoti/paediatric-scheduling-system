8d5559109e185865c8afad73d98c1c0e53a25789
Arthur Mwang'onda <44365037+arthurnamz@users.noreply.github.com>
2024-02-20 15:50:16 +0200

    updates

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('doc/doctor_list', [App\Http\Controllers\DoctorController::class, 'index'])->name('doctor_list');
